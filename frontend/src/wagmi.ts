68cedb5e42025056a2bc3a9bea82710ad32744f8
grimsby00 <xxvlhjcb3362@hotmail.com>
2025-09-29 20:11:12 +0800

    delete useless

--- frontend/src/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sepolia } from 'wagmi/chains';
-
-export const config = getDefaultConfig({
-  appName: 'FHE Customer DB',
-  projectId: 'YOUR_PROJECT_ID', // Get this from WalletConnect Cloud
-  chains: [sepolia],
-  ssr: false,
-});
